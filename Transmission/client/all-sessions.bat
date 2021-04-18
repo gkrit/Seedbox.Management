c09fd5b936a4da894d70b8d8089e7ef0a2ffc800
gkrit <giorgos.kritikos@hotmail.com>
2021-04-18 15:23:55 +0300

    fix error in path of transmission-qt.exe

--- Transmission/client/all-sessions.bat
+++ Transmission/client/all-sessions.bat
@@ -1,5 +1,5 @@
 @echo off
 
-start C:\"Program Files"\Transmission\transmission-qt.exe --config-dir .\seedbox1
-start C:\"Program Files"\Transmission\transmission-qt.exe --config-dir .\seedbox2
-start C:\"Program Files"\Transmission\transmission-qt.exe --config-dir .\seedbox3
+start .\qt\transmission-qt.exe --config-dir .\seedbox1
+start .\qt\transmission-qt.exe --config-dir .\seedbox2
+start .\qt\transmission-qt.exe --config-dir .\seedbox3
